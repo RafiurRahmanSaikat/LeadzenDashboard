06e92a2fd54c0e803174628e3f5dc272d6de63be
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-02-03 00:00:38 +0600

    Displaying Data Done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { GetData } from './assets/Utilities';
+import { GetData } from '../assets/Utilities';
 import Info from './Info';
-
+import Components from './Components.css';
 const Dashboard = () => {
     const [DATA, setDATA] = useState(null)
     const [page, setPage] = useState(1)
@@ -10,25 +10,40 @@ const Dashboard = () => {
         GetData()
             .then(response => {
                 setDATA(response)
-                console.log(response)
+                // console.log(response)
             })
     }, [])
 
 
     return (
+
         <>
-            <section className='flex flex-col'>
+            <section>
+
                 {
                     DATA ? DATA.slice(page * 3 - 3, page * 3).map(items => <Info key={items.id} data={items}></Info>) : <p className='text-9xl text-red-700'>LOADING DATA...</p>
                 }
+
+
+
             </section>
-            <section>
+            <div className='pagination'>
+
                 {
                     DATA &&
-                    [...Array(Math.ceil(DATA.length / 3))].map((_, i) => <button onClick={()=>setPage(i)} className="btn btn-warning m-2">{++i}</button>)
+
+                    [...Array(Math.ceil(DATA.length / 3))].map((_, i) =>
+                        <button
+
+                            key={i} onClick={() => setPage(i)}
+                            className="  btn btn-warning m-2">{++i}
+
+                        </button>)
                 }
-            </section>
+
+            </div>
         </>
+
     );
 };
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Info = ({ data }) => {
-    console.log("INFO", data)
-    const { name,id, username, email, phone, website, compnay } = data
+    const { name, id, username, email, phone, website, company } = data
     const { street, suite, city, zipcode, geo } = data.address
+    console.log(company.name);
+    const [mood, setMood] = useState(false)
 
     return (
-        <section className=' bg-green-300 rounded-xl mt-6  '>
-            <div className='grid grid-cols-5 h-24 items-center'>
-                <p>{id}</p>
-                <p>{name}</p>
-                <p>{email}</p>
-                <p>{phone}</p>
-                <p>{website}</p>
+        <section className=' bg-gray-100 rounded-xl mt-6  '>
+            <div className='grid grid-cols-5  h-24 items-center'>
+
+                <p>{company.name}</p>
+
+                <div>
+                    <p className='font-semibold'>CONTACT</p>
+                    {name}
+                </div>
+                <div>
+                    <p className='font-semibold'>CITY</p>
+                    {city}
+                </div>
+                <div>
+                    <p className='font-semibold'>STREET</p>
+                    {street}
+                </div>
+                {/* <p>{phone}</p> */}
+
+                <button className={`w-32 mx-auto rounded-full ${mood ? "btn-xs btn-error" : "btn-xs btn-success"}`}
+                    onClick={() => setMood(!mood)}>{mood ? "Hide Details" : "View Details"}
+                </button>
             </div>
+            {
+                mood && <section className='p-8'>
+                    <article className='text-start space-y-4 '>
+                        <p className='font-bold'>Description</p>
+                        <div>
+                            <p>{company.catchPhrase}  {company.bs}</p>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <div className=' '>
+                             <p>{company.catchPhrase}</p>
+                             <p>{company.catchPhrase}</p>
+                             <p>{company.catchPhrase}</p>
+                             <p>{company.catchPhrase}</p>
+                             <p>{company.catchPhrase}   </p>
+                                
+                            </div>
+                            <div>
+                               <p> {company.bs}</p>
+                               <p> {company.bs}</p>
+                               <p> {company.bs}</p>
+                               <p> {company.bs}</p>
+                               <p> {company.bs}</p>
+                                
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                </section>
+            }
         </section>
     );
 };
